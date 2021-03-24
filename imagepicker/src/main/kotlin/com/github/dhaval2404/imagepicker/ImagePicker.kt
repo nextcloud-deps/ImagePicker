7d74732cfb41cb2103b02b6efad0a9c5317fd96a
tobiasKaminsky <tobias@kaminsky.me>
2021-03-24 10:08:42 +0100

    Add "FILE" Provider to directly crop existing file

--- imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/ImagePicker.kt
+++ imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/ImagePicker.kt
@@ -38,6 +38,7 @@ open class ImagePicker {
         internal const val EXTRA_ERROR = "extra.error"
         internal const val EXTRA_FILE_PATH = "extra.file_path"
         internal const val EXTRA_MIME_TYPES = "extra.mime_types"
+        internal const val EXTRA_FILE = "extra.file"
 
         /**
          * Use this to use ImagePicker in Activity Class
@@ -78,7 +79,7 @@ open class ImagePicker {
         private var fragment: Fragment? = null
 
         // Image Provider
-        private var imageProvider = ImageProvider.BOTH
+        private var imageProvider = ImageProvider.GALLERY_OR_CAMERA
 
         // Mime types restrictions for gallery. by default all mime types are valid
         private var mimeTypes: Array<String> = emptyArray()
@@ -147,6 +148,11 @@ open class ImagePicker {
         // @Deprecated("Please use provider(ImageProvider.GALLERY) instead")
         fun galleryOnly(): Builder {
             this.imageProvider = ImageProvider.GALLERY
+            return this
+        }
+        
+        fun fileOnly() : Builder {
+            this.imageProvider = ImageProvider.FILE
             return this
         }
 
@@ -270,7 +276,7 @@ open class ImagePicker {
          * Start Image Picker Activity
          */
         fun start(reqCode: Int) {
-            if (imageProvider == ImageProvider.BOTH) {
+            if (imageProvider == ImageProvider.GALLERY_OR_CAMERA) {
                 // Pick Image Provider if not specified
                 showImageProviderDialog(reqCode)
             } else {
@@ -292,21 +298,10 @@ open class ImagePicker {
         /**
          * Get Intent
          */
-        fun createIntent(onResult: (Intent) -> Unit) {
-            if (imageProvider == ImageProvider.BOTH) {
-                DialogHelper.showChooseAppDialog(
-                    activity,
-                    object : ResultListener<ImageProvider> {
-                        override fun onResult(t: ImageProvider?) {
-                            t?.let {
-                                imageProvider = it
-                                imageProviderInterceptor?.invoke(imageProvider)
-                                onResult(createIntent())
-                            }
-                        }
-                    },
-                    dismissListener
-                )
+        fun start(completionHandler: ((resultCode: Int, data: Intent?) -> Unit)? = null) {
+            if (imageProvider == ImageProvider.GALLERY_OR_CAMERA) {
+                // Pick Image Provider if not specified
+                showImageProviderDialog(completionHandler)
             } else {
                 onResult(createIntent())
             }
